0fb55bc5d4bca4318afc3a890e71817bdadbb782
urvirath0d <74901176+urvirath0d@users.noreply.github.com>
2020-11-24 19:56:23 +0530

    30 days 30 hours

---
+++ day2.py
@@ -0,0 +1,11 @@
+a=b=c=1
+print(a, b, c)
+print(a/10)
+print(b*50)
+print(c+60)
+u="maths"
+print(u)
+print(u.replace("h","G"))
+(a, b) = (2, 4.5)
+print("(a, b)= ",(a,b))
+print("(a,b)=", (float(a), int(b)))
